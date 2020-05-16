eaed7c34553abacd21dd0e859a2fd8ff66ef1bdb
Daniel <hbs315@gmail.com>
2020-05-16 20:52:09 +0700

    Deploy by command

---
+++ precache-manifest.4f9e21932987fce788c3828a1f2be334.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e645c7b05d5fa46ade373fb9ffdc564d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2e31ee8b7362d8e259d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ebb7ca11c0d9461d1975",
+    "url": "/static/css/main.05a44390.chunk.css"
+  },
+  {
+    "revision": "b2e31ee8b7362d8e259d",
+    "url": "/static/js/2.28739a78.chunk.js"
+  },
+  {
+    "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
+    "url": "/static/js/2.28739a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb7ca11c0d9461d1975",
+    "url": "/static/js/main.d305512c.chunk.js"
+  },
+  {
+    "revision": "4d7d9e1a7e886cccc438",
+    "url": "/static/js/runtime-main.568774a1.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
